0db263f42e9b3c912f7cd58a5fd25fa61c2e08be
konrad <ktalaga90@gmail.com>
2021-05-24 20:58:29 +0100

    canSellTobbacoIfOver18 passed

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -10,12 +10,14 @@ public class PlaygroundTest {
     Playground playground;
     private Visitor visitor_1;
     private Visitor visitor_2;
+    private Visitor visitor_3;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
         visitor_1 = new Visitor(21,210.00,1000.00);
         visitor_2 = new Visitor(13,140.00,300.00);
+        visitor_3 = new Visitor(9,120.00,100.00);
 
     }
 
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,23 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    private Visitor visitor_1;
+    private Visitor visitor_2;
+    private Visitor visitor_3;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor_1 = new Visitor(21,210.00,1000.00);
+        visitor_2 = new Visitor(13,140.00,300.00);
+        visitor_3 = new Visitor(9,120.00,100.00);
     }
 
     @Test
@@ -27,5 +34,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canSellTobbacoIfOver18(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor_1));
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor_2));
     }
 }
